34e03e4e79cb2842bb67dda72a48b52c4c0456cf
Moutasim3282 <moutasimamjad3282@gmail.com>
2023-07-19 13:19:58 +0500

    Change title and name

---
+++ lib/assesment_1/fi_moutasim.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FiMoutasim extends StatelessWidget {
+  const FiMoutasim({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Moutasim"),
+      ),
+      body: const Center(
+        child: Text("Moutasim name"),
+      ),
+    );
+  }
+}
