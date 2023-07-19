c22f89d061963692bb3e5e7a6c91907b7612d18a
Moutasim3282 <moutasimamjad3282@gmail.com>
2023-07-19 12:47:49 +0500

    Change title and center text

---
+++ lib/assesment_1/fi_moutasim.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FIMoutasim extends StatelessWidget {
+  const FIMoutasim({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Moutasim"),
+      ),
+      body: const Center(
+        child: Text("Hello! I am Moutasim"),
+      ),
+    );
+  }
+}
